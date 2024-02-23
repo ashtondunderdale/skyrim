4d777a5bb18412d2f0add781f068cec825a20fe4
Ashton Dunderdale <ashtondunderdale@gmail.com>
2024-02-23 14:52:02 +0000

    added race picker

--- Data.cs
+++ Data.cs
@@ -1,6 +1,265 @@
 ﻿namespace skyrim;
 
-internal class Data
+internal class Race
 {
-    static List
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+
+    public int Health = 100;
+
+    public int Stamina = 100;
+    public int Magicka = 100;
+    public int Smithing { get; set; }
+    public int HeavyArmour { get; set; }
+    public int Block { get; set; }
+    public int TwoHanded { get; set; }
+    public int OneHanded { get; set; }
+    public int Archery { get; set; }
+    public int LightArmor { get; set; }
+    public int Sneak { get; set; }
+    public int Lockpicking { get; set; }
+    public int Pickpocket { get; set; }
+    public int Speech { get; set; }
+    public int Alchemy { get; set; }
+    public int Illusion { get; set; }
+    public int Conjuration { get; set; }
+    public int Destruction { get; set; }
+    public int Restoration { get; set; }
+    public int Alteration { get; set; }
+    public int Enchantment { get; set; }
+
+    public static List<Race> Races { get; } = new List<Race>
+    {
+        new Race { 
+            Name = "Altmer", 
+
+            Magicka = 150, // yeah i know 
+
+            Smithing = 15,
+            HeavyArmour = 15,
+            Block = 15,
+            TwoHanded = 15,
+            OneHanded = 15,
+            Archery = 15,
+            LightArmor = 15,
+            Sneak = 15,
+            Lockpicking = 15,
+            Pickpocket = 15,
+            Speech = 15,
+            Alchemy = 15,
+            Illusion = 25,
+            Conjuration = 20,
+            Destruction = 20,
+            Restoration = 20,
+            Alteration = 20,
+            Enchantment = 20,
+        },
+
+        new Race { 
+            Name = "Argonian",
+
+            Smithing = 15,
+            HeavyArmour = 15,
+            Block = 15,
+            TwoHanded = 15,
+            OneHanded = 15,
+            Archery = 15,
+            LightArmor = 20,
+            Sneak = 20,
+            Lockpicking = 25,
+            Pickpocket = 20,
+            Speech = 15,
+            Alchemy = 15,
+            Illusion = 15,
+            Conjuration = 15,
+            Destruction = 15,
+            Restoration = 20,
+            Alteration = 20,
+            Enchantment = 15,
+        },
+
+        new Race {
+            Name = "Bosmer",
+
+            Smithing = 15,
+            HeavyArmour = 15,
+            Block = 15,
+            TwoHanded = 15,
+            OneHanded = 15,
+            Archery = 25,
+            LightArmor = 20,
+            Sneak = 20,
+            Lockpicking = 20,
+            Pickpocket = 20,
+            Speech = 15,
+            Alchemy = 20,
+            Illusion = 15,
+            Conjuration = 15,
+            Destruction = 15,
+            Restoration = 15,
+            Alteration = 15,
+            Enchantment = 15,
+        },
+
+        new Race {
+            Name = "Breton",
+
+            Smithing = 15,
+            HeavyArmour = 15,
+            Block = 15,
+            TwoHanded = 15,
+            OneHanded = 15,
+            Archery = 15,
+            LightArmor = 15,
+            Sneak = 15,
+            Lockpicking = 15,
+            Pickpocket = 15,
+            Speech = 20,
+            Alchemy = 20,
+            Illusion = 20,
+            Conjuration = 25,
+            Destruction = 15,
+            Restoration = 20,
+            Alteration = 20,
+            Enchantment = 15,
+        },
+
+        new Race {
+            Name = "Dunmer",
+
+            Smithing = 15,
+            HeavyArmour = 15,
+            Block = 15,
+            TwoHanded = 15,
+            OneHanded = 15,
+            Archery = 15,
+            LightArmor = 20,
+            Sneak = 20,
+            Lockpicking = 15,
+            Pickpocket = 15,
+            Speech = 15,
+            Alchemy = 20,
+            Illusion = 20,
+            Conjuration = 15,
+            Destruction = 25,
+            Restoration = 15,
+            Alteration = 20,
+            Enchantment = 15,
+        },
+
+        new Race {
+            Name = "Imperial",
+
+            Smithing = 15,
+            HeavyArmour = 20,
+            Block = 20,
+            TwoHanded = 15,
+            OneHanded = 20,
+            Archery = 15,
+            LightArmor = 15,
+            Sneak = 15,
+            Lockpicking = 15,
+            Pickpocket = 15,
+            Speech = 15,
+            Alchemy = 15,
+            Illusion = 15,
+            Conjuration = 15,
+            Destruction = 20,
+            Restoration = 25,
+            Alteration = 15,
+            Enchantment = 20,
+        },
+
+        new Race {
+            Name = "Khajiit",
+
+            Smithing = 15,
+            HeavyArmour = 15,
+            Block = 15,
+            TwoHanded = 15,
+            OneHanded = 20,
+            Archery = 20,
+            LightArmor = 15,
+            Sneak = 25,
+            Lockpicking = 20,
+            Pickpocket = 20,
+            Speech = 15,
+            Alchemy = 20,
+            Illusion = 15,
+            Conjuration = 15,
+            Destruction = 15,
+            Restoration = 15,
+            Alteration = 15,
+            Enchantment = 15,
+        },
+
+        new Race {
+            Name = "Nord",
+
+            Smithing = 20,
+            HeavyArmour = 15,
+            Block = 20,
+            TwoHanded = 25,
+            OneHanded = 20,
+            Archery = 15,
+            LightArmor = 20,
+            Sneak = 15,
+            Lockpicking = 15,
+            Pickpocket = 15,
+            Speech = 20,
+            Alchemy = 15,
+            Illusion = 15,
+            Conjuration = 15,
+            Destruction = 15,
+            Restoration = 15,
+            Alteration = 15,
+            Enchantment = 15,
+        },
+
+        new Race {
+            Name = "Orsimer",
+
+            Smithing = 20,
+            HeavyArmour = 25,
+            Block = 20,
+            TwoHanded = 20,
+            OneHanded = 20,
+            Archery = 15,
+            LightArmor = 15,
+            Sneak = 15,
+            Lockpicking = 15,
+            Pickpocket = 15,
+            Speech = 15,
+            Alchemy = 15,
+            Illusion = 15,
+            Conjuration = 15,
+            Destruction = 15,
+            Restoration = 15,
+            Alteration = 15,
+            Enchantment = 20,
+        },
+
+        new Race {
+            Name = "Redguard",
+
+            Smithing = 20,
+            HeavyArmour = 15,
+            Block = 120,
+            TwoHanded = 15,
+            OneHanded = 25,
+            Archery = 20,
+            LightArmor = 15,
+            Sneak = 15,
+            Lockpicking = 15,
+            Pickpocket = 15,
+            Speech = 15,
+            Alchemy = 15,
+            Illusion = 15,
+            Conjuration = 15,
+            Destruction = 20,
+            Restoration = 15,
+            Alteration = 20,
+            Enchantment = 15,
+        },
+    };
 }
--- Program.cs
+++ Program.cs
@@ -7,12 +7,64 @@ internal class Program
 {
     static void Main()
     {
+        ShowCharacterCreation();
+
         Console.WriteLine("Play Intro? (y)");
         string? playIntro = Console.ReadLine();
 
         if (playIntro == "y")
         {
             Scripts.PlayIntroScript();
+        }
+
+    }
+
+    public static Race ShowCharacterCreation() 
+    {
+        Console.ForegroundColor = ConsoleColor.DarkGray;
+        int activeRaceIndex = 0;
+        Race activeRace = Race.Races[activeRaceIndex];
+
+        while (true)
+        {
+            foreach (var race in Race.Races)
+            {
+                if (race == activeRace)
+                {
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(race.Name);
+                    Console.ForegroundColor = ConsoleColor.DarkGray;
+                }
+                else
+                {
+                    Console.WriteLine(race.Name);
+                }
+            }
+
+            ConsoleKeyInfo input = Console.ReadKey(intercept: true);
+
+            if (input.Key == ConsoleKey.UpArrow)
+            {
+                if (activeRaceIndex - 1 > -1)
+                {
+                    activeRaceIndex--;
+                    activeRace = Race.Races[activeRaceIndex];
+                }
+            }
+            else if (input.Key == ConsoleKey.DownArrow)
+            {
+                if (activeRaceIndex < Race.Races.Count - 1)
+                {
+                    activeRaceIndex++;
+                    activeRace = Race.Races[activeRaceIndex];
+                }
+            }
+            else if (input.Key == ConsoleKey.Enter) 
+            {
+                return Race.Races[activeRaceIndex];
+            }
+
+            Console.Clear();
         }
     }
 }
